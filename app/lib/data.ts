e75bb3f5f0339992792f832cde7b16425c37078b
shu-bc <shu@baseconnect.in>
2024-07-11 22:11:20 +0900

    card data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -38,37 +38,25 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+  const numberOfCustomers = await prisma.customer.count();
+  const numberOfInvoices = await prisma.invoice.count();
+  const invoiceStatus = await prisma.invoice.groupBy({
+    by: ['status'],
+    _sum: {
+      amount: true
+    },
+    where: {
+      status: {
+        in: ['paid', 'pending']
+      }
+    }
+  })
 
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
-    ]);
-
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
-
-    return {
-      numberOfCustomers,
-      numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
-    };
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch card data.');
+  return {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices: formatCurrency(invoiceStatus.find(status => status.status === 'paid')?._sum.amount ?? 0),
+    totalPendingInvoices: formatCurrency(invoiceStatus.find(status => status.status === 'pending')?._sum.amount ?? 0)
   }
 }
 
