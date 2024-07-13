e5c8cf8e5edbbfdbcf0d72636b3bf3b4901385b6
shu-bc <shu@baseconnect.in>
2024-07-13 13:35:36 +0900

    invoices search

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -77,33 +77,28 @@ export async function fetchFilteredInvoices(
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
-  try {
-    const invoices = await sql<InvoicesTable>`
+  const result = await prisma.$queryRaw<InvoicesTable>`
       SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
+        i.id as id,
+        i.amount as amount,
+        i.date as date,
+        i.status as status,
+        c.name as name,
+        c.email as email,
+        c.image_url as image_url
+      FROM "Invoice" i
+      JOIN "Customer" c ON i.customer_id = c.id
       WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
+        c.name ILIKE ${`%${query}%`} OR
+        c.email ILIKE ${`%${query}%`} OR
+        i.amount::text ILIKE ${`%${query}%`} OR
+        i.date::text ILIKE ${`%${query}%`} OR
+        i.status ILIKE ${`%${query}%`}
+      ORDER BY i.date DESC
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    `;
+  `
 
-    return invoices.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoices.');
-  }
+  return result
 }
 
 export async function fetchInvoicesPages(query: string) {
