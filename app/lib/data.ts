34d8cdd680634632d3d953af26c33f7eafc982db
shu-bc <shu@baseconnect.in>
2024-07-13 20:06:51 +0900

    fix pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,7 @@ import {
 import { formatCurrency } from './utils';
 import { prisma } from './prisma';
 import { LatestInvoice } from './definitions';
+import { countReset } from 'console';
 
 export async function fetchRevenue() {
   console.log('Fetching revenue data...');
@@ -102,24 +103,20 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  try {
-    const count = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
+  const countResult = await prisma.$queryRaw<{count: Number}[]>`
+    SELECT COUNT(*)
+        FROM "Invoice" i
+        JOIN "Customer" c ON i.customer_id = c.id
+        WHERE
+          c.name ILIKE ${`%${query}%`} OR
+          c.email ILIKE ${`%${query}%`} OR
+          i.amount::text ILIKE ${`%${query}%`} OR
+          i.date::text ILIKE ${`%${query}%`} OR
+          i.status ILIKE ${`%${query}%`}
+  `
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-    return totalPages;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch total number of invoices.');
-  }
+  const totalPages = Math.ceil(Number(countResult[0].count) / ITEMS_PER_PAGE);
+  return totalPages;
 }
 
 export async function fetchInvoiceById(id: string) {
