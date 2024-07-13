cd8423a16d35e184444e4a70e9ea7ae057ac2e34
shu-bc <shu@baseconnect.in>
2024-07-13 21:05:25 +0900

    wip editing

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -120,28 +120,18 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  try {
-    const data = await sql<InvoiceForm>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
-    `;
+  const data = await prisma.invoice.findMany({
+    where: {
+      id: id
+    },
+  })
 
-    const invoice = data.rows.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
-    }));
+  const invoice = data.map((invoice) => ({
+    ...invoice,
+    amount: invoice.amount / 100
+  }));
 
-    return invoice[0];
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
-  }
+  return invoice[0];
 }
 
 export async function fetchCustomers() {
